a5a9cfa736e77aae2ef04645d22e7b9959fd374b
Benocci <francesco.beno00gmail.com>
2023-06-22 16:18:04 +0200

    contract development

--- P2P_Project/contracts/BattleShipGame.sol
+++ P2P_Project/contracts/BattleShipGame.sol
@@ -2,7 +2,33 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract BattleShipGame {
-  uint256[] public gameList; //array of game's ID
-  
-  
+    struct gameInfo {
+        address creator;
+        address joiner;
+        uint256 boardSize;
+        uint256 shipNum;
+        uint256 ethAmount;
+        bool openGame;
+    }
+
+    mapping(uint256 => gameInfo) public gameList; //map of game's ID
+
+    event GameCreated(
+        uint256 indexed _gameId,
+        uint256 boardSize,
+        uint256 _shipNum
+    );
+
+    function createGame(uint256 _boardSize, uint256 _shipNum) public {
+        uint256 gameIdToAdd;
+
+        gameList[gameIdToAdd] = gameInfo(
+            msg.sender,
+            address(0),
+            _boardSize,
+            _shipNum,
+            0,
+            true
+        );
+    }
 }
